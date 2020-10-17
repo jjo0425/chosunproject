afc459da2027fec887b10988ef8e9fb7999be233
jjo0425 <cowddong0425@hanmail.net>
2020-10-18 08:22:21 +0900

    2마무리

---
+++ Assets/Sungnyemun/Script/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public DialogueManager manager;
+
+    private void Awake()
+    {
+        TriggerDialogue();
+    }
+
+    public void TriggerDialogue ()
+    {
+        manager.StartDialogue(dialogue);
+    }
+}
