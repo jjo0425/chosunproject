e4c12e7c275df31a2667ba13314bf4c47c040673
jjo0425 <cowddong0425@hanmail.net>
2020-10-25 15:48:58 +0900

    1수정

---
+++ Assets/Character/ARTapToPlaceObject.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+    public Camera cam;
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager manager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        manager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+      
+      
+    }
+
+    private void PlaceOdject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = cam.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        manager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }        
+}
+
+
---
+++ Assets/Sungnyemun/Script/DialogueManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text dialogueText;
+
+  private Queue<string> sentences;
+    
+    public void StartDialogue (Dialogue dialogue)
+    {
+        if (sentences == null) sentences = new Queue<string>();
+        Debug.Log(dialogue.sentences.Length);
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence); 
+        }
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    
+
+        void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
+
+ 
